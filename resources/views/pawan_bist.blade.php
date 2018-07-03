43c1d5496b5e738193f492c785604ca35c8fcb92
Pawan-Bist <pawanbist666@outlook.com>
2018-07-03 10:04:01 +0545

    first attempt in deployment

---
+++ resources/views/pawan_bist.blade.php
@@ -0,0 +1,386 @@
+<!DOCTYPE html>
+<html lang="en">
+
+  <head>
+
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title>Pawan Bist</title>
+    <link rel="icon" type="image/png" href="{{asset('assets/icons/cv.png')}}"/>
+
+    <!-- Bootstrap core CSS -->
+    <link href="{{asset('assets/css/bootstrap.min.css')}}" rel="stylesheet">
+
+    <!-- Custom fonts for this template -->
+    <link href="https://fonts.googleapis.com/css?family=Saira+Extra+Condensed:100,200,300,400,500,600,700,800,900" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i,800,800i" rel="stylesheet">
+    <link href="{{asset('assets/css/font-awesome.min.css')}}" rel="stylesheet">
+    <!-- <link href="{{asset('assets/css/devicons.min.css')}}" rel="stylesheet"> -->
+    <link href="{{asset('assets/css/devicon.min.css')}}"  rel="stylesheet">
+    <!-- <link href="{{asset('assets/fonts/devicons.ttf')}}" rel="stylesheet">
+    <link href="{{asset('assets/fonts/devicons.woff')}}" rel="stylesheet"> -->
+    <link href="{{asset('assets/fonts/devicon.ttf')}}" rel="stylesheet">
+    <link href="{{asset('assets/fonts/devicon.woff')}}" rel="stylesheet">
+    <link href="{{asset('assets/css/simple-line-icons.css')}}" rel="stylesheet">
+
+    <!-- Custom styles for this template -->
+    <link href="{{asset('assets/css/resume.min.css')}}" rel="stylesheet">
+
+  </head>
+
+  <body id="page-top">
+
+    <nav class="navbar navbar-expand-lg navbar-dark bg-primary fixed-top" id="sideNav">
+      <a class="navbar-brand js-scroll-trigger" href="#page-top">
+        <span class="d-block d-lg-none">Top</span>
+        <span class="d-none d-lg-block">
+          <img class="img-fluid img-profile rounded-circle mx-auto mb-0" src="assets/img/profile.jpeg" alt="">
+        </span>
+      </a>
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav">
+          <li>
+            <ul class="list-inline list-social-icons mb-0" style="padding-left: 70px">
+              <li class="list-inline-item">
+                <a href="https://www.facebook.com/pawan.bist.73">
+                  <span class="fa-stack fa-li">
+                    <i class="fa fa-circle fa-stack-2x"></i>
+                    <i class="fa fa-facebook fa-stack-1x fa-inverse"></i>
+                  </span>
+                </a>
+              </li>
+              <li class="list-inline-item">
+                <a href="https://github.com/Pawan-Bist">
+                  <span class="fa-stack fa-li">
+                    <i class="fa fa-circle fa-stack-2x"></i>
+                    <i class="fa fa-github fa-stack-1x fa-inverse"></i>
+                  </span>
+                </a>
+              </li>
+              <li class="list-inline-item">
+                <a href="https://www.linkedin.com/in/pawan-bist-473425167/">
+                  <span class="fa-stack fa-li">
+                    <i class="fa fa-circle fa-stack-2x"></i>
+                    <i class="fa fa-linkedin fa-stack-1x fa-inverse"></i>
+                  </span>
+                </a>
+              </li>
+            </ul>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link js-scroll-trigger" href="#about">About</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link js-scroll-trigger" href="#experience">Experience</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link js-scroll-trigger" href="#education">Education</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link js-scroll-trigger" href="#skills">Skills</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link js-scroll-trigger" href="#interests">Interests</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link js-scroll-trigger" href="#awards">Awards</a>
+          </li>
+        </ul>
+      </div>
+    </nav>
+
+    <div class="container-fluid p-0">
+
+      <section class="resume-section p-3 p-lg-5 d-flex d-column" id="about">
+        <div class="my-auto">
+          <h1 class="mb-0">Pawan
+            <span class="text-primary">Bist</span>
+          </h1>
+          <div class="subheading mb-5">Bhansi, Mahendranagar · Kanchanpur, Nepal
+            <br/>
+            Current: Panga, Kirtipur · Kathmandu, Nepal · (+977)-9848789349 ·
+            <a href="mailto:pawanbist666@outlook.com">pawanbist666@outlook.com</a>
+          </div>
+
+          <p class="mb-5">Leapfrog certified PHP/Laravel developer</p>
+          <div class="subheading mb-3">Programming Languages &amp; Tools</div>
+          <ul class="list-inline list-icons">
+            <li class="list-inline-item">
+              <i class="devicon-html5-plain"></i>
+            </li>
+            <li class="list-inline-item">
+              <i class="devicon-css3-plain"></i>
+            </li>
+            <li class="list-inline-item">
+              <i class="devicon-javascript-plain"></i>
+            </li>
+            <li class="list-inline-item">
+              <i class="devicon-jquery-plain"></i>
+            </li>
+            <li class="list-inline-item">
+              <i class="devicon-bootstrap-plain"></i>
+            </li>
+            <li class="list-inline-item">
+              <i class="devicon-php-plain"></i>
+            </li>
+            <li class="list-inline-item">
+              <i class="devicon-laravel-plain"></i>
+            </li>
+            <li class="list-inline-item">
+              <i class="devicon-plain-plain"></i>
+            </li>
+            <li class="list-inline-item">
+              <i class="devicon-bower-plain"></i>
+            </li>
+            <li class="list-inline-item">
+              <i class="devicon-heroku-original"></i>
+            </li>
+          </ul>
+          <ul class="list-inline list-icons">
+            <li class="list-inline-item">
+              <i class="devicon-c-plain"></i>
+            </li>
+            <li class="list-inline-item">
+              <i class="devicon-cplusplus-plain"></i>
+            </li>
+            <li class="list-inline-item">
+              <i class="devicon-mysql-plain"></i>
+            </li>
+          </ul>
+        </div>
+      </section>
+
+      <section class="resume-section p-3 p-lg-5 d-flex flex-column" id="experience">
+        <div class="my-auto">
+          <h2 class="mb-5">Experience</h2>
+
+          <div class="resume-item d-flex flex-column flex-md-row mb-5">
+            <div class="resume-content mr-auto">
+              <h3 class="mb-0">None</h3>
+              <div class="subheading mb-3"> </div>
+              <p>Highly interested and motivated to work as a part of team.</p>
+            </div>
+            <div class="resume-date text-md-right">
+              <span class="text-primary"></span>
+            </div>
+          </div>
+        </div>
+
+      </section>
+
+      <section class="resume-section p-3 p-lg-5 d-flex flex-column" id="education">
+        <div class="my-auto">
+          <h2 class="mb-5">Education and Training</h2>
+
+          <div class="resume-item d-flex flex-column flex-md-row mb-5">
+            <div class="resume-content mr-auto">
+              <h3 class="mb-0">Leapfrog Academy</h3>
+              <div class="subheading mb-3">PHP/Laravel-JS</div>
+              <div>Certified </div>
+            </div>
+            <div class="resume-date text-md-right">
+              <span class="text-primary">2018</span>
+            </div>
+          </div>
+
+          <div class="resume-item d-flex flex-column flex-md-row mb-5">
+            <div class="resume-content mr-auto">
+              <h3 class="mb-0">Tribhuwan University(TU)</h3>
+              <div class="subheading mb-3">Bachelor in Mechanical Engineering</div>
+              <div>IOE-Thapathali Campus, Thapathali Kathmandu</div>
+              <p>65%</p>
+            </div>
+            <div class="resume-date text-md-right">
+              <span class="text-primary">2013-2017</span>
+            </div>
+          </div>
+
+          <div class="resume-item d-flex flex-column flex-md-row">
+            <div class="resume-content mr-auto">
+              <h3 class="mb-0">Higher Secondary Education Board(HSEB)</h3>
+              <div class="subheading mb-3">+2 Science</div>
+              <p>
+                Radiant Higher Secondary School
+                <br/>
+                65.60%
+              </p>
+            </div>
+            <div class="resume-date text-md-right">
+              <span class="text-primary">2011-2013</span>
+            </div>
+          </div>
+
+          <div class="resume-item d-flex flex-column flex-md-row">
+            <div class="resume-content mr-auto">
+              <h3 class="mb-0">School Leaving Certifcate(SLC)</h3>
+              <div class="subheading mb-3">+2 Science</div>
+              <p>
+                United Public Secondary School
+                <br/>
+                76.38%
+              </p>
+            </div>
+            <div class="resume-date text-md-right">
+              <span class="text-primary">2011-2013</span>
+            </div>
+          </div>
+
+        </div>
+      </section>
+
+      <section class="resume-section p-3 p-lg-5 d-flex flex-column" id="skills">
+        <div class="my-auto">
+          <h2 class="mb-5">Skills</h2>
+
+          <div class="resume-item d-flex flex-column flex-md-row mb-5">
+            <div class="resume-content mr-auto">
+              <h3 class="subheading mb-3">PHP</h3>
+                <ul class="fa-ul mb-0">
+                  <li>
+                    <i class="fa-li fa fa-check"></i>
+                    Object Oriented Programming Concepts
+                  </li>
+                  <li>
+                    <i class="fa-li fa fa-check"></i>
+                    Design Patterns (MVC, Factory, Repository)
+                  </li>
+                  <li>
+                    <i class="fa-li fa fa-check"></i>
+                    Laravel Framework
+                  </li>
+                  <li>
+                    <i class="fa-li fa fa-check"></i>
+                    Basic CRUD projects
+                  </li>
+                  <li>
+                    <i class="fa-li fa fa-check"></i>
+                    Web scrapping using curl
+                  </li>
+                  <li>
+                    <i class="fa-li fa fa-check"></i>
+                    ORM and Filtering
+                  </li>
+                </ul>
+            </div>
+          </div>
+          <div class="resume-item d-flex flex-column flex-md-row mb-5" id="skill">
+            <div class="resume-content mr-auto">
+              <h3 class="subheading mb-3">Front End</h3>
+              <ul class="fa-ul mb-0">
+                <li>
+                  <i class="fa-li fa fa-check"></i>
+                  HTML 5
+                </li>
+                <li>
+                  <i class="fa-li fa fa-check"></i>
+                  CSS 3
+                </li>
+                <li>
+                  <i class="fa-li fa fa-check"></i>
+                  JavaScript
+                </li>
+                <li>
+                  <i class="fa-li fa fa-check"></i>
+                  Jquery
+                </li>
+                <li>
+                  <i class="fa-li fa fa-check"></i>
+                  Bootstrap
+                </li>
+              </ul>
+            </div>
+          </div>
+          <div class="resume-item d-flex flex-column flex-md-row mb-5" id="skill">
+            <div class="resume-content mr-auto">
+              <h3 class="subheading mb-3">Databases</h3>
+              <ul class="fa-ul mb-0">
+                <li>
+                  <i class="fa-li fa fa-check"></i>
+                  MySQL
+                </li>
+                <li>
+                  <i class="fa-li fa fa-check"></i>
+                  PostgreSQL
+                </li>
+              </ul>
+            </div>
+          </div>
+          <div class="resume-item d-flex flex-column flex-md-row mb-5" id="skill">
+            <div class="resume-content mr-auto">
+              <h3 class="subheading mb-3">Others</h3>
+              <ul class="fa-ul mb-0">
+                <li>
+                  <i class="fa-li fa fa-check"></i>
+                  GIT
+                </li>
+                <li>
+                  <i class="fa-li fa fa-check"></i>
+                  Linux
+                </li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </section>
+
+      <section class="resume-section p-3 p-lg-5 d-flex flex-column" id="interests">
+        <div class="my-auto">
+          <h2 class="mb-5">Interests</h2>
+          <ul class="fa-ul mb-0">
+                <li>
+                  <i class="fa-li fa fa-check"></i>
+                  Code
+                </li>
+                <li>
+                  <i class="fa-li fa fa-check"></i>
+                  Music
+                </li>
+                <li>
+                  <i class="fa-li fa fa-check"></i>
+                  Movies
+                </li>
+                <li>
+                  <i class="fa-li fa fa-check"></i>
+                  Games
+                </li>
+          </ul>
+        </div>
+      </section>
+
+      <section class="resume-section p-3 p-lg-5 d-flex flex-column" id="awards">
+        <div class="my-auto">
+          <h2 class="mb-5">Awards &amp; Certifications</h2>
+          <ul class="fa-ul mb-0">
+            <li>
+              <i class="fa-li fa fa-trophy text-warning"></i>
+              3 times winner winner chicken dinner in PUGB mobile ;-)
+            </li>
+            <li>
+              <i class="fa-li fa fa-trophy text-warning"></i>
+              Leapfrog Certified<sup></sup>
+            </li>
+          </ul>
+        </div>
+      </section>
+
+    </div>
+
+    <!-- Bootstrap core JavaScript -->
+    <script src="{{asset('assets/js/jquery.min.js')}}"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.1.1/js/bootstrap.bundle.min.js"></script>
+
+    <!-- Plugin JavaScript -->
+    <script src="{{asset('assets/js/jquery.easing.min.js')}}"></script>
+
+    <!-- Custom scripts for this template -->
+    <script src="{{asset('assets/js/resume.min.js')}}"></script>
+
+  </body>
+
+</html>
